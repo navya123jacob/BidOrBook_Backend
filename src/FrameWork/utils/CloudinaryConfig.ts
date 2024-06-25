d4d395fd319bc9131a5df316f80cdb7374910297
navya123jacob <navyatjacob@gmail.com>
2024-06-25 11:02:10 +0530

    corrections

---
+++ src/FrameWork/utils/CloudinaryConfig.ts
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dvgwqkegd', 
+  api_key: '133934819117526', 
+  api_secret: 'tNf2c4aiiYrHXcvPFoASS1Eg_Gk',
+  secure:true
+});
+export {cloudinary}
