499d2ee6d7490d02faf33cb7dabfc059e538ff34
navya123jacob <navyatjacob@gmail.com>
2024-07-05 20:00:22 +0530

    configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'backend',
-      script: 'src/index.ts',
-      watch: true,
-      ignore_watch: ['node_modules', 'logs'],
-      instances: 1,
-      exec_mode: 'fork',
-      interpreter: '/usr/bin/ts-node',
-      interpreter_args: '-r tsconfig-paths/register',
-      autorestart: true,
-      max_restarts: 10,
-      env: {
-        NODE_ENV: 'production',
-        PORT: 8888,
+    apps: [
+      {
+        name: 'backend',
+        script: 'src/index.ts',
+        watch: true,
+        ignore_watch: ['node_modules', 'logs'],
+        instances: 1,
+        exec_mode: 'fork',
+        interpreter: '/usr/bin/ts-node',
+        autorestart: true,
+        max_restarts: 10,
+        env: {
+          NODE_ENV: 'production',
+          PORT: 8888,
+        },
       },
-    },
-  ],
-};
+    ],
+  };
+  
